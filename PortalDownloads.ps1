7b106b0bd20ac27a7882c64b7f6f4bc32b0df97f
anwather <anwather@microsoft.com>
2015-11-13 12:26:22 +1100

    Initial Commit

---
+++ PortalDownloads.ps1
@@ -0,0 +1,21 @@
+﻿Configuration PortalDownload
+    {
+
+    Node localhost
+        {
+
+        File SetupFile
+            {
+            DestinationPath = "C:\Temp"
+            SourcePath = "https://download.microsoft.com/download/B/3/7/B37F4D01-F382-421C-8D3C-7F3BA2CBCCF3/Setup.zip"
+            Force = $true
+            }
+
+        Archive ExtractFile
+            {
+            Path = "C:\Temp\Setup.zip"
+            Destination = "C:\Temp\Portal\"
+            Force = $true
+            }
+        }
+    }
