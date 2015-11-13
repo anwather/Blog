5379912a6eea69f8a49ab5c0dcea947a2a4e43b3
anwather <anwather@microsoft.com>
2015-11-13 13:10:26 +1100

    remote file working

--- PortalDownloads.ps1
+++ PortalDownloads.ps1
@@ -1,14 +1,15 @@
 ﻿Configuration PortalDownload
     {
 
+    Import-DSCResource -ModuleName xPSDesiredStateConfiguration
+
     Node localhost
         {
 
         File SetupFile
             {
+            Uri = "https://raw.githubusercontent.com/anwather/Blog/Dev/Setup.zip"
             DestinationPath = "C:\Temp"
-            SourcePath = "https://download.microsoft.com/download/B/3/7/B37F4D01-F382-421C-8D3C-7F3BA2CBCCF3/Setup.zip"
-            Force = $true
             }
 
         Archive ExtractFile
