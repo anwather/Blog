a03af3d15ae4a628f1b4fd1402580c20e989f067
anwather <anwather@microsoft.com>
2015-11-19 16:28:02 +1100

    modified script loop

--- PortalWebServer.ps1
+++ PortalWebServer.ps1
@@ -1,54 +1,26 @@
 ﻿Configuration PortalWebServer
     {
+
+    Param(
+        [string[]]$Feature)
     # Need to change to add a foreach loop
     Node localhost
         {
 
-        WindowsFeature WebServer
+        foreach ($WindowsFeature in $Feature)
             {
-            Ensure="Present"
-            Name="Web-Server"
-            }
+            WindowsFeature $Feature
+                {
+                Ensure = "Present"
+                Name = $Feature
+                }
+        
 
-        WindowsFeature NETASP
-            {
-            Ensure="Present"
-            Name="NET-Framework-45-ASPNET"
-            DependsOn = "[WindowsFeature]WebServer"
-            }
-
-        WindowsFeature WebAuth
-            {
-            Ensure="Present"
-            Name="Web-Windows-Auth"
-            DependsOn = "[WindowsFeature]WebServer"
-            }
-        WindowsFeature BasicAuth
-            {
-            Ensure="Present"
-            Name="Web-Basic-Auth"
-            DependsOn = "[WindowsFeature]WebServer"
-            }
-        WindowsFeature ASP
-            {
-            Ensure="Present"
-            Name="Web-ASP"
-            DependsOn = "[WindowsFeature]WebServer"
-            }
-        WindowsFeature ASPNET
-            {
-            Ensure="Present"
-            Name="Web-ASP-Net"
-            DependsOn = "[WindowsFeature]WebServer"
-            }
-        WindowsFeature ASPNET45
-            {
-            Ensure="Present"
-            Name="Web-ASP-Net45"
-            DependsOn = "[WindowsFeature]WebServer"
             }
         }
-      }
+    }
 
-PortalWebServer
+
+PortalWebServer -Feature 'WebServer','NETASP','WebAuth','BasicAuth','ASP','ASPNET','ASPNET45'
+Start-DscConfiguration -Path .\PortalWebServer -Wait -Verbose
         
