251b77896ae6b9f3cd0f8543357ef7cda1db7f15
anwather <anwather@microsoft.com>
2015-12-09 13:42:12 +0800

    new script

---
+++ PortalWebServerV2.ps1
@@ -0,0 +1,21 @@
+﻿Configuration PortalWebServer
+    {
+
+    Param(
+        [string[]]$Feature)
+    # Need to change to add a foreach loop
+    Node PortalServer
+        {
+
+        foreach ($WindowsFeature in $Feature)
+            {
+            WindowsFeature $Feature
+                {
+                Ensure = "Present"
+                Name = $Feature
+                }
+        
+
+            }
+        }
+    }
